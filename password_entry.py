baaf1f81614fa1ea986b325edea9f0f10c54ff9d
Curtis <curtis.lynch@my.jcu.edu.au>
2019-03-23 23:19:40 +1000

    Added Password Length Checker

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,11 @@
-"""
-Curtis Lynch
-"""
+password = input("What is the password? ")
+
+min_length = 3
+password_length = len(password)
+
+while password_length < min_length:
+    print("Invalid")
+    password = input(">")
+    password_length = len(password)
+else:
+    print("*" * password_length)
